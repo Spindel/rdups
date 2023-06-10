49a6fada1df2afe5b2caad0931217db2abe2b484
D.S. Ljungmark <spider@skuggor.se>
2023-06-10 22:28:54 +0200

    Remove rayon and use my own home-hacked thread-pool

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use std::env;
 use std::fs::File;
 use std::io;
 use std::path::PathBuf;
+use std::sync::mpsc;
+use std::thread;
 use std::vec::Vec;
 
 // Walkdir
@@ -23,14 +25,21 @@ fn main() -> Result<(), io::Error> {
         }
     };
 
-    // Walk all files.
-    let files = walk_files(path)?;
+    let (paths_tx, paths_rx) = mpsc::channel();
+    let (hash_tx, hash_rx) = mpsc::channel();
 
-    // Group all files by size.
-    let group_by_size = group_files_by_size(files);
+    // Make path an owned copy for the walker thread
+    let path: String = path.into();
+
+    // Walk all files.
+    thread::spawn(move || walk_files(path, paths_tx).expect("Failure is an option"));
+
+    // Split out the filtering file actor, it buffers data from the file walker and then sends
+    // paths that should be hashed out as they arrive.
+    thread::spawn(move || filter_files(paths_rx, hash_tx));
 
     // Group all files by checksum.
-    let group_by_checksum = group_files_by_checksum(group_by_size)?;
+    let group_by_checksum = group_files_by_checksum(hash_rx)?;
 
     // Get all duplicated files, grouped by checksum.
     let dups = duplicated_files(group_by_checksum);
@@ -46,61 +55,96 @@ fn main() -> Result<(), io::Error> {
     Ok(())
 }
 
+/// Filter files as they come in, based on their metadata.
+fn filter_files(rx: mpsc::Receiver<(u64, PathBuf)>, tx: mpsc::Sender<PathBuf>) {
+    let mut groups: BTreeMap<u64, Vec<Option<PathBuf>>> = BTreeMap::new();
+    while let Ok((file_len, path)) = rx.recv() {
+        {
+            let files = groups.entry(file_len).or_default();
+            if files.is_empty() {
+                files.push(Some(path));
+            } else {
+                tx.send(path).unwrap();
+                files.push(None);
+                /*
+                for f in files {
+                    if let Some(f) = f.take() {
+                        tx.send(f).unwrap();
+                    }
+                }*/
+                // I'm still learning how the iterable approach is supposed to look, this is the
+                // same as above
+                files
+                    .iter_mut()
+                    .filter_map(|x| x.take())
+                    .for_each(|path| tx.send(path).unwrap());
+            }
+        }
+    }
+}
+
 // walk_files, walk all files in all subdirectories.
 // Return a vector with size and file path.
-fn walk_files(path: &str) -> Result<Vec<(u64, Option<PathBuf>)>, io::Error> {
-    let mut files: Vec<(u64, Option<PathBuf>)> = Vec::new();
-
+fn walk_files(path: String, tx: mpsc::Sender<(u64, PathBuf)>) -> Result<(), io::Error> {
     for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {
         if entry.file_type().is_file() {
             let file_len = entry.metadata()?.len();
             if file_len != 0 {
-                files.push((file_len, Some(entry.into_path())));
+                tx.send((file_len, entry.into_path()))
+                    .expect("tx send failure");
             }
         }
     }
-    Ok(files)
-}
-
-// group_files_by_size group all files by file size. Using a
-// vector with size and path.
-fn group_files_by_size(files: Vec<(u64, Option<PathBuf>)>) -> BTreeMap<u64, Vec<Option<PathBuf>>> {
-    let mut groups: BTreeMap<u64, Vec<Option<PathBuf>>> = BTreeMap::new();
-
-    for (size, path) in files {
-        groups.entry(size).or_default().push(path);
-    }
-    groups
-}
-
-fn filter_file_list(files: BTreeMap<u64, Vec<Option<PathBuf>>>) -> Vec<PathBuf> {
-    // Filter the files to check into a list of paths only, flattening the hashmap.
-    let files_to_check: Vec<_> = files
-        .into_iter()
-        .filter(|(_, paths)| paths.len() > 1)
-        .map(|(_, paths)| paths.into_iter().flatten())
-        .flatten()
-        .collect();
-    files_to_check
+    Ok(())
 }
 
 // group_files_by_checksum group all files by checksum. Using blake3 to calculate a
 // checksum for the files.
 fn group_files_by_checksum(
-    files: BTreeMap<u64, Vec<Option<PathBuf>>>,
+    filechan: mpsc::Receiver<PathBuf>,
 ) -> Result<HashMap<String, Vec<PathBuf>>, io::Error> {
+    use std::sync::{Arc, Mutex};
+
+    // Channels for the worker-threads that hash the files
+    let (tx, rx) = mpsc::channel();
+
+    // Wrap the readable channel in an mutex with reference counting,
+    // so the threads can read them as wanted
+    let lx = Arc::new(Mutex::new(filechan));
+    for _ in 0..64 {
+        let rx = lx.clone();
+        let tx = tx.clone();
+        thread::spawn(move || {
+            loop {
+                let path = {
+                    if let Ok(rx) = rx.lock() {
+                        rx.recv()
+                    } else {
+                        // Failed to lock / Poisoned.
+                        // Another thread died while holding the rx lock
+                        return;
+                    }
+                };
+                if let Ok(path) = path {
+                    let sum = blake3_checksum(&path);
+                    if tx.send((sum, path)).is_err() {
+                        // Unable to send data to receiever?
+                        // Main thread may be dead for some reason
+                        return;
+                    }
+                }
+                // Channel hung up, no more data
+                else {
+                    return;
+                }
+            }
+        });
+    }
+    // Drop the original tx, or we will always have a living sender
+    drop(tx);
+
     let mut groups: HashMap<String, Vec<PathBuf>> = HashMap::new();
-    use rayon::prelude::*;
-
-    let files_to_check = filter_file_list(files);
-
-    // Hash all files as (Result<sum>, path)
-    let mut hashes: Vec<_> = files_to_check
-        .into_par_iter()
-        .map(|path| (blake3_checksum(&path), path))
-        .collect();
-
-    for (sum, path) in hashes.drain(..) {
+    while let Ok((sum, path)) = rx.recv() {
         groups.entry(sum?).or_default().push(path);
     }
     Ok(groups)
